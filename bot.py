d8e850964aaaf7a822abed1d241cebe0e94e5c52
y1220asdf <35912060+y1220asdf@users.noreply.github.com>
2021-07-20 02:12:46 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,13 +16,29 @@ async def on_ready():
 async def on_message(message):
     #排除自己的訊息，避免陷入無限循環
     if message.author == client.user:
-            return
+             return
         #如果包含 ping，機器人回傳 pong
     if message.content == '早安':
         await message.channel.send('早安')
 
     if message.content =='晚安':
         await message.channel.send('晚安')
+
+    if message.content =='笨蛋':
+        await message.channel.send('對不起啦....')
+    if message.content =='/weareroc':
+        await message.channel.send('中華民國萬歲，三民主義統一中國，我們是自由民主中國。')
+    if message.content =='/menu' or message.content == '/選單':
+        text= '''/say 參數 ---說
+                 /menu 選單    
+                 早安 回覆早安
+                 晚安 回覆晚安
+                 笨蛋 回覆對不起啦...
+                 我好帥,我好可愛,我好棒-刪除信息並回覆
+                 /weareroc 中華民國
+                 /狀態 參數 改變bot狀態'''
+        
+        await message.channel.send(text)
 
     if message.content.startswith('/狀態'):
         tmp = message.content.split(' ')
@@ -49,15 +65,14 @@ async def on_message(message):
             await message.channel.send("你要我工三小??")
         else:
             await message.channel.send(tmp[1])
-
-    if '想要' in message.content:
-        print('偵測到了')
-        await message.delete() 
-
-@client.command()
+    await client.process_commands(message)
+        
+@client.command(name='clear')
 async def clear(ctx ,num:int):
     print('cleared')
     await ctx.channel.purge(limit=num+1)
+@client.command(name='kick')
+async def kick(ctx, member : discord.Member, *,reason=None):
+    await member.kick(reason=reason)
 
 client.run(TOKEN) #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
-                        
